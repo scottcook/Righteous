6962ce7cc3c6458cd8bc00984671834b2e4ba063
Ash Warren <ash@driftlab.com>
2025-05-09 12:57:42 -0500

    In progress logo animation.

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -11,8 +11,12 @@ const sectionRef = ref(null);
 const imageRef = ref(null);
 const copyRef = ref(null);
 const descriptionRef = ref(null);
+const handRef = ref(null);
+const logoRef = ref(null);
 
 let resizeTimeout = null;
+let descriptionSplit = null;
+let logoSplit = null;
 
 const getClipSettings = () => {
     const screenWidth = window.innerWidth;
@@ -25,14 +29,18 @@ const getClipSettings = () => {
     return { horizontalCropPercent, copyHeight, navHeight };
 };
 
-const setupScrollAnimation = () => {
+const setupScrollAnimation = async () => {
+    await nextTick();
+
     const { horizontalCropPercent, copyHeight, navHeight } = getClipSettings();
 
     ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    descriptionSplit && descriptionSplit.revert();
 
     gsap.set(mediaRef.value, { clipPath: 'inset(0% 0% 0% 0% round 0px)' });
     gsap.set(imageRef.value, { scale: 1 });
     gsap.set(descriptionRef.value, { opacity: 1 });
+    gsap.set(handRef.value, { opacity: 0 });
 
     const tl = gsap.timeline({
         scrollTrigger: {
@@ -48,20 +56,29 @@ const setupScrollAnimation = () => {
     tl.to(mediaRef.value, {
         clipPath: `inset(${navHeight}px ${horizontalCropPercent}% ${copyHeight}px ${horizontalCropPercent}% round 8px)`,
         ease: 'power2.out',
-    }).to(
-        imageRef.value,
-        {
-            scale: 1.1,
-            ease: 'power1.in',
-        },
-        '<'
-    );
+    })
+        .to(
+            imageRef.value,
+            {
+                scale: 1.1,
+                ease: 'power1.in',
+            },
+            '<'
+        )
+        .to(
+            logoRef.value,
+            {
+                yPercent: 5,
+                ease: 'power1.out',
+            },
+            '<'
+        );
 
-    const split = new SplitText(descriptionRef.value, { type: 'lines' });
+    descriptionSplit = new SplitText(descriptionRef.value, { type: 'lines' });
 
     tl.fromTo(
-        split.lines,
-        { opacity: 0, y: 20 },
+        descriptionSplit.lines,
+        { opacity: 0, y: 30 },
         {
             opacity: 1,
             y: 0,
@@ -70,6 +87,41 @@ const setupScrollAnimation = () => {
         },
         '>-0.2'
     );
+
+    tl.fromTo(
+        handRef.value,
+        { opacity: 0, y: 50, rotation: 10 },
+        {
+            opacity: 1,
+            y: 0,
+            rotation: 0,
+            ease: 'back.inOut',
+        },
+        '>-0.4'
+    );
+};
+
+const setupLogoAnimation = async () => {
+    await nextTick();
+
+    logoSplit && logoSplit.revert();
+
+    gsap.set(logoRef.value, { opacity: 1 });
+
+    logoSplit = new SplitText(logoRef.value, { type: 'chars' });
+
+    gsap.fromTo(
+        logoSplit.chars,
+        { opacity: 0, scale: 2 },
+        {
+            opacity: 1,
+            scale: 1,
+            stagger: 0.1,
+            ease: 'power2.inOut',
+            duration: 1,
+        },
+        '0.2'
+    );
 };
 
 const handleResize = () => {
@@ -77,18 +129,19 @@ const handleResize = () => {
     resizeTimeout = setTimeout(() => {
         setupScrollAnimation();
         ScrollTrigger.refresh();
-    }, 200);
+    }, 250);
 };
 
 onMounted(async () => {
-    await nextTick();
-    requestAnimationFrame(setupScrollAnimation);
+    await setupScrollAnimation();
+    await setupLogoAnimation();
     window.addEventListener('resize', handleResize);
 });
 
 onUnmounted(() => {
     window.removeEventListener('resize', handleResize);
     clearTimeout(resizeTimeout);
+    descriptionSplit && descriptionSplit.revert();
 });
 
 defineExpose({ updateClipAndScale: setupScrollAnimation });
@@ -98,17 +151,21 @@ defineExpose({ updateClipAndScale: setupScrollAnimation });
     <section ref="sectionRef" class="relative w-screen h-screen pointer-events-none">
         <div ref="copyRef" class="absolute bottom-0 left-0 right-0 w-full">
             <div class="w-full grid grid-cols-wrapper">
-                <div class="col-main pb-12 pt-9">
-                    <p ref="descriptionRef" class="text-brand-gray max-w-[680px] font-helveticaDisplay font-medium text-[28px] leading-9">
+                <div class="relative col-main pb-12 pt-9 bg-brand-cream">
+                    <p ref="descriptionRef" class="text-brand-gray max-w-[530px] lg:max-w-[680px] font-helveticaDisplay font-medium text-[24px] lg:text-[28px] leading-7 lg:leading-9">
                         <span class="text-black">We’re Righteous.</span><br />
                         A small team of product and agency veterans, crafting clean strategy, smart UX, and tight code for brands and startups who want results -- without the pitch theater.
                     </p>
+                    <div ref="handRef" class="mix-blend-exclusion absolute right-0 bottom-0 top-0">
+                        <img src="@/assets/images/skelehand.png" alt="Skelehand" class="h-full w-auto transform scale-110 opacity-10 md:opacity-100" />
+                    </div>
                 </div>
             </div>
         </div>
         <div class="overflow-hidden w-full h-full relative">
             <div ref="mediaRef" class="w-full h-full">
                 <img ref="imageRef" src="@/assets/images/placeholder-image.jpg" alt="Placeholder" class="w-full h-full object-cover object-center" />
+                <div ref="logoRef" class="absolute inset-0 w-full h-full flex items-center justify-center"><h2 class="font-canela text-white text-[13vw]">Righteous</h2></div>
             </div>
         </div>
     </section>
